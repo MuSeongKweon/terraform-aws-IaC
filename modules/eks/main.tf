7f3a0f68bf9fda0ce44a356017e9407cccac803f
MuSeongKweon <89920576+MuSeongKweon@users.noreply.github.com>
2026-03-22 03:48:47 +0900

    Update scheduling nodes

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -77,8 +77,8 @@ resource "aws_eks_node_group" "main" {
   subnet_ids = var.private_subnet_ids
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 5
+    max_size     = 5
     min_size     = 1
   }
 
