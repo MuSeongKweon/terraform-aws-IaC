386fe28c66e556f287d9714599f78197d95f6968
MuSeongKweon <juniyo9766@gmail.com>
2026-03-22 02:53:00 +0900

    fix(eks): scale node group to resolve pod scheduling issue

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -77,8 +77,8 @@ resource "aws_eks_node_group" "main" {
   subnet_ids = var.private_subnet_ids
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
+    desired_size = 2
+    max_size     = 3
     min_size     = 1
   }
 
